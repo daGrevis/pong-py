fff4cb0b1b6a329ea01c443497ccfb305ae99d71
Raitis Stengrevics <dagrevis@gmail.com>
2015-03-18 20:09:37 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/brython-dev/brython/releases/download/3.1.0/Brython3.1.0-20150301-090019.tar.gz -O- | tar -xz Brython3.1.0-20150301-090019/brython.js
+mv Brython3.1.0-20150301-090019/brython.js brython.js
+rm -rf Brython3.1.0-20150301-090019/
